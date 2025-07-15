f75cd94dc20736794a8dc97d906d9354415d0eac
EngineerHuayhua <huayhuadonatelo@gmail.com>
2025-07-15 00:59:11 -0400

    actualizacion de functions

--- 06-functions/function/function.go
+++ 06-functions/function/function.go
@@ -1,6 +1,9 @@
 package function
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Operation int
 
@@ -22,20 +25,52 @@ func RepeatString(value string, increment int) {
 	}
 }
 
-func Calc(op Operation, x, y float64) (float64, bool) {
+func Calc(op Operation, x, y float64) (float64, error) {
 	switch op {
 	case SUM:
-		return x + y, false
+		return x + y, nil
 	case SUB:
-		return x - y, false
+		return x - y, nil
 	case DIV:
 		if y == 0 {
-			return 0, true
+			return 0, errors.New("y mustn't be zero")
 		}
-		return x / y, false
+		return x / y, nil
 	case MUL:
-		return x * y, false
+		return x * y, nil
 	default:
-		return 0, true
+		return 0, errors.New("has been an error")
 	}
+}
+
+func Split(v int) (x, y int) {
+	x = v * 4 / 9
+	y = v - x
+	return
+}
+
+func MSum(values ...float64) float64 {
+	var sum float64
+	for _, v := range values {
+		sum += v
+	}
+	return sum
+}
+
+func MOperations(op Operation, values ...float64) (float64, error) {
+	if len(values) == 0 {
+		return 0, errors.New("there aren't values")
+	}
+
+	result := values[0]
+
+	for _, v := range values[1:] {
+		var err error
+		result, err = Calc(op, result, v)
+		if err != nil {
+			return 0, err
+		}
+	}
+	return result, nil
+
 }
--- 06-functions/main.go
+++ 06-functions/main.go
@@ -13,9 +13,50 @@ func main() {
 
 	fmt.Println()
 	v, err := function.Calc(function.SUM, 3, 6)
-	fmt.Println("Sum:", v, "- Error:", err)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println("Sum:", v)
+	}
+
+	v, err = function.Calc(function.DIV, 3, 0)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println("Div:", v)
+	}
 
 	fmt.Println()
-	v, err = function.Calc(function.DIV, 3, 0)
-	fmt.Println("Div:", v, "- Error:", err)
+	x, y := function.Split(20)
+	fmt.Println("value 1:", x, "value 2:", y)
+
+	fmt.Println()
+	v = function.MSum(23, 45, 67, 89, 12, 34, 56, 78, 90, 11, 22, 33, 44, 55, 66, 77, 88, 99)
+	fmt.Println("multy sum:", v)
+
+	v, err = function.MOperations(function.SUM, 2, 7, 1)
+	fmt.Println("multy sum: ", v, "error: ", err)
+
+	v, err = function.MOperations(function.SUB, 10, 5, 2, 1, 3)
+	fmt.Println("multy sub: ", v, "error: ", err)
+
+	v, err = function.MOperations(function.MUL, 10, 5, 2, 1, 3)
+	fmt.Println("multy mul: ", v, "error: ", err)
+
+	v, err = function.MOperations(function.DIV, 10, 5, 2, 1, 3)
+	fmt.Println("multy div: ", v, "error: ", err)
+
+	fmt.Println()
+	fn := function.FactoryOperation(function.SUM)
+	v = fn(10, 5)
+	fmt.Println("Factory sum:", v)
+	fn = function.FactoryOperation(function.SUB)
+	v = fn(10, 5)
+	fmt.Println("Factory sub:", v)
+	fn = function.FactoryOperation(function.MUL)
+	v = fn(10, 5)
+	fmt.Println("Factory mul:", v)
+	fn = function.FactoryOperation(function.DIV)
+	v = fn(10, 5)
+	fmt.Println("Factory div:", v)
 }
